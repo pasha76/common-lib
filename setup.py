61627e10c0a73786a903b85e51b1d45d4407c8d5
pasha76 <gunduz.tolga@googlemail.com>
2024-08-31 18:41:35 +0300

    deneme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='common',  # Name of your package
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=['build', 'build.*',"common.egg*"]),  # Exclude 'build' folder and its subpackages
     install_requires=[
        "pymysql",
