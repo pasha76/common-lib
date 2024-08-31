ba7f1acaa76d0cf173d31e0c1b6f4d3a8a65e326
pasha76 <gunduz.tolga@googlemail.com>
2024-09-01 01:49:34 +0300

    new pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blushy',  # Name of your package
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(exclude=['build', 'build.*',"blushy.egg*"]),  # Exclude 'build' folder and its subpackages
     install_requires=[
        "pymysql",
