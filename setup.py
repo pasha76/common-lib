5eb609bf83dddbc0120699018cf4d54277bc9dbf
pasha76 <gunduz.tolga@googlemail.com>
2024-08-31 17:22:22 +0300

    Update setup.py to exclude specific folders and include necessary packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='common',  # Name of your package
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['build', 'build.*']),  # Exclude 'build' folder and its subpackages
     install_requires=[
        "pymysql",
