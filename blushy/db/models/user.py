77ac492959bd4efc67ba9a4535b8b4c1336cbed6
pasha76 <gunduz.tolga@googlemail.com>
2024-09-04 17:24:51 +0300

    similar finder changed

--- blushy/db/models/user.py
+++ blushy/db/models/user.py
@@ -46,6 +46,6 @@ class User(Base):
     def is_vendor(self):
         return self.vendor_id is not None
 
-    def to_dict(self):
+    def to_dict(self,ignore_keys=[]):
         return {c.key: getattr(self, c.key)
-                for c in inspect(self).mapper.column_attrs}
+                for c in inspect(self).mapper.column_attrs if c.key not in ignore_keys}
