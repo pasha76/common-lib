ea5d9987bf57e112988430dc416fd2c8718db333
pasha76 <gunduz.tolga@googlemail.com>
2024-09-06 15:29:09 +0300

    items are done

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blushy',  # Name of your package
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(exclude=['build', 'build.*',"blushy.egg*"]),  # Exclude 'build' folder and its subpackages
     install_requires=[
        "pymysql",
@@ -20,7 +20,8 @@ setup(
        "numpy",
        "sentence-transformers",
        "google-cloud",
-       "google-cloud-storage"
+       "google-cloud-storage",
+       "imagehash"
     ],
     include_package_data=True,
     description='A utility package for common functionalities',
