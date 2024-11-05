95f4cfcb5b158a30f002549c118243a2d0cdea37
pasha76 <gunduz.tolga@googlemail.com>
2024-11-05 13:16:36 +0400

    db pool

--- blushy/db/models/base.py
+++ blushy/db/models/base.py
@@ -14,7 +14,12 @@ def init_db(engine_url="mysql+pymysql://root:tolga1194031@35.184.196.46/blushyv2
     :param engine_url: The database URL.
     :return: Sessionmaker instance.
     """
-    engine = create_engine(engine_url)
+    engine = create_engine(engine_url,
+                           pool_size=20,  # Increase from 5
+                            max_overflow=30,  # Increase from 10
+                            pool_timeout=60,  # Increase timeout from 30
+                            pool_pre_ping=True  # Add connection health check)
+                            )
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
     return sessionmaker(bind=engine)
