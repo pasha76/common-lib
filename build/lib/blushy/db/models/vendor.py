5ad08f1e623c9a22e0f7e691ce932b91b07133d9
pasha76 <gunduz.tolga@googlemail.com>
2025-03-27 12:14:31 +0300

    vendor

--- build/lib/blushy/db/models/vendor.py
+++ build/lib/blushy/db/models/vendor.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey,DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey,DateTime,Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from blushy.db.models import Base
@@ -16,7 +16,11 @@ class Vendor(Base):
     country = relationship('Country', back_populates='vendors')
     items = relationship('Item', back_populates='vendor')
     users = relationship('User', back_populates='vendor')
+    product_feed_url = Column(String(1000))
+    product_feed_last_updated = Column(DateTime)
+    product_mapping=Column(Text)
     vendor_statuses = relationship('VendorStatus', back_populates='vendor')  
+
 
     created_at = Column(DateTime, default=datetime.utcnow)
 
